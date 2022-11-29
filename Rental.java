b332b5cad0149c44f1e10d88b1511bb3f2c4d41a
RodolfoLessa <rodolfolessa.eng@gmail.com>
2022-11-29 16:26:38 -0300

    Commit 4

--- Rental.java
+++ Rental.java
@@ -15,4 +15,26 @@ public class Rental {
     public Movie getMovie() {
        return _movie;
     }
+
+    public double getCharge() {     
+       double charge = 0;
+       
+       switch (getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          charge += 2;
+          if (getDaysRented() > 2)
+             charge += (getDaysRented() - 2) * 1.5;
+          break;
+       case Movie.NEW_RELEASE:
+          charge += getDaysRented() * 3;
+          break;
+       case Movie.CHILDRENS:
+          charge += 1.5;
+          if (getDaysRented() > 3)
+             charge += (getDaysRented() - 3) * 1.5;
+             break;
+       }
+    
+       return charge;
+    }
  }
