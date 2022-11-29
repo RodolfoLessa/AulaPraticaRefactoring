f448ea07238d1b89af9abb67b21442cb12541170
RodolfoLessa <rodolfolessa.eng@gmail.com>
2022-11-29 17:42:51 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -3,14 +3,7 @@ public abstract class Price {
 
     public abstract double getCharge(int daysRented);
 
-    public int getFrequentRenterPoints(int daysRented) { 
-       int frequentRenterPoints = 1;
-       
-       // add bonus for a two day new release rental
-       if ((getPriceCode() == Movie.NEW_RELEASE) &&
-             daysRented > 1) frequentRenterPoints ++;
-    
-       return frequentRenterPoints;
+    public int getFrequentRenterPoints(int daysRented) {
+      return 1;
     }
-
  }
